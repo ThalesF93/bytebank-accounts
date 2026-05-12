13392ca78f574b319c76a58abde76502ea9c55e9
Thales Fernandes <thalesgarcezf@gmail.com>
2026-05-12 16:30:57 -0300

    configurando redis

--- src/main/java/br/com/bytebank/accounts/api/dtos/client/response/CustomerClientResponseDTO.java
+++ src/main/java/br/com/bytebank/accounts/api/dtos/client/response/CustomerClientResponseDTO.java
@@ -1,10 +1,11 @@
 package br.com.bytebank.accounts.api.dtos.client.response;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 public record CustomerClientResponseDTO(
         UUID id,
 
         String name
-) {
+) implements Serializable {
 }
--- src/main/java/br/com/bytebank/accounts/infrastructure/config/FeignErrorDecoder.java
+++ src/main/java/br/com/bytebank/accounts/infrastructure/config/FeignErrorDecoder.java
@@ -3,12 +3,13 @@ package br.com.bytebank.accounts.infrastructure.config;
 
 
 import br.com.bytebank.accounts.domain.exception.ResourceNotFoundException;
+import br.com.bytebank.accounts.domain.exception.ServiceUnavailableException;
 import feign.FeignException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import org.apache.coyote.BadRequestException;
 
-import javax.naming.ServiceUnavailableException;
+
 
 public class FeignErrorDecoder implements ErrorDecoder {
 
@@ -17,7 +18,7 @@ public class FeignErrorDecoder implements ErrorDecoder {
         return switch (response.status()){
             case 400 -> new BadRequestException("Invalid Request");
             case 404 -> new ResourceNotFoundException("Resource not found");
-            case 500 -> new ServiceUnavailableException("Service Unavailable");
+            case 503 -> new ServiceUnavailableException("Service Unavailable");
             default -> new FeignException.FeignClientException(
                     response.status(), response.reason(), response.request(), null, null
             );
